7cc729feaec76012642d9d1156fe89b2ead8c58b
nttrung90 <nguyentrung2590@gmail.com>
2026-02-28 23:31:03 +0700

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -1,7 +1,8 @@
 const config = {
-  siteName: process.env.SITE_NAME || "Gia Phả OS",
+  siteName: process.env.SITE_NAME || "Gia Phả Dòng họ Nguyễn Thiệu",
   exampleEmail: process.env.EXAMPLE_EMAIL || "",
   examplePassword: process.env.EXAMPLE_PASSWORD || "",
 };
 
 export default config;
+
